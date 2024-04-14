de5657c2c9850dd795beecbf31404b76597ce0e3
Takanori Fujiwara <taka.fuji.fujiwara@gmail.com>
2024-04-14 23:49:27 +0200

    installation guide for venv and python3.12

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='multilens',
-      version=0.01,
-      packages=[''],
-      package_dir={'': '.'},
-      install_requires=['numpy', 'scipy', 'sklearn'],
-      py_modules=['multilens', 'multilens_utils'])
+setup(
+    name="multilens",
+    version=0.01,
+    packages=[""],
+    package_dir={"": "."},
+    install_requires=["numpy", "scipy", "scikit-learn"],
+    py_modules=["multilens", "multilens_utils"],
+)
